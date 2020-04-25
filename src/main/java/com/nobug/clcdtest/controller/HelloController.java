ded2380adcf12a41d404a3ebaa8cf49a57c99d12
oleolema <1075056431@qq.com>
2020-04-25 15:56:57 +0800

    asdf

--- src/main/java/com/nobug/clcdtest/controller/HelloController.java
+++ src/main/java/com/nobug/clcdtest/controller/HelloController.java
@@ -25,7 +25,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello() {
-        return "hello world success pull docker !!!";
+        return "hello world success!!!";
     }
 
 }
